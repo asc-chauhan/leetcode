7ee3c71ab2ed801e7c527c19c80783174c9d71b2
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2023-09-08 05:46:16 +0530

    Time: 32 ms (72.16%), Space: 17.8 MB (42.39%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -9,17 +9,54 @@
  * };
  */
 //Upvote  and Comment
+
 class Solution {
 public:
+    ListNode* reverse(ListNode* head){
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* nxt=NULL;
+        while(curr){
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+        }
+        return prev;
+    }
+    
     void reorderList(ListNode* head) {
-        if(!head || !head->next || !head->next->next) 
-            return;
-        ListNode* penultimate = head;
-        while (penultimate->next->next) 
-            penultimate = penultimate->next;
-        penultimate->next->next = head->next;
-        head->next = penultimate->next;
-        penultimate->next = NULL;
-        reorderList(head->next->next);
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast and fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* second=reverse(slow->next);
+        slow->next=NULL;
+        ListNode* first=head;
+        while(second){
+            ListNode* temp1=first->next;
+            ListNode* temp2=second->next;
+            first->next=second;
+            second->next=temp1;
+            first=temp1;
+            second=temp2;
+        }
     }
 };
+
+// class Solution {
+// public:
+//     void reorderList(ListNode* head) {
+//         if(!head || !head->next || !head->next->next) 
+//             return;
+//         ListNode* penultimate = head;
+//         while (penultimate->next->next) 
+//             penultimate = penultimate->next;
+//         penultimate->next->next = head->next;
+//         head->next = penultimate->next;
+//         penultimate->next = NULL;
+//         reorderList(head->next->next);
+//     }
+// };
