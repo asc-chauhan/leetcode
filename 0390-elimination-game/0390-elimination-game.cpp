15d0b96b1b6a7bd37e5b5dbb51cc28f1e51d7c81
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2023-09-10 20:33:55 +0530

    Time: 3 ms (73%), Space: 5.9 MB (93.86%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -1,15 +1,27 @@
+// class Solution {
+// public:
+//     int lastRemaining(int n) {
+//         int start=1,diff=1,no=n;
+//         bool left2right=true;
+//         while(no>1){
+//             if(left2right||no%2==1)
+//                 start+=diff;
+//             diff*=2;
+//             no/=2;
+//             left2right=!left2right;
+//         }
+//         return start;
+//     }
+// };
+
 class Solution {
 public:
+    int f(int n) {
+        if (n <= 2) return n - 1;
+        if (n % 2 == 1) return n - 2 - 2 * f((n-1)/2);
+        else return n - 1 - 2 * f(n/2);
+    }
     int lastRemaining(int n) {
-        int start=1,diff=1,no=n;
-        bool left2right=true;
-        while(no>1){
-            if(left2right||no%2==1)
-                start+=diff;
-            diff*=2;
-            no/=2;
-            left2right=!left2right;
-        }
-        return start;
+        return f(n) + 1;
     }
 };
