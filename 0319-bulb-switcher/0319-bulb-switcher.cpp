8317bd4ae464ca4fc39cd307cd9e83e2c7735e83
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2023-09-13 16:10:30 +0530

    Time: 0 ms (100%), Space: 6.4 MB (15.66%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,6 +1,18 @@
+// class Solution {
+// public:
+//     int bulbSwitch(int n) {
+//         return sqrt(n);
+//     }
+// };
 class Solution {
 public:
     int bulbSwitch(int n) {
-        return sqrt(n);
+        long long i=1;
+        int ct=0;
+        while((i*i)<=n){
+            i++;
+            ct++;
+        }
+        return ct;
     }
 };
