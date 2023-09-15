57433308fac3438723e92a29126b7ebd9dc12a63
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2023-09-15 23:14:52 +0530

    Time: 12 ms (48.8%), Space: 10.7 MB (25.86%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
+        if(numRows <= 1)
+            return s;
         string ans;
         vector<string> v(numRows);
         int i = 0, n = s.size();
