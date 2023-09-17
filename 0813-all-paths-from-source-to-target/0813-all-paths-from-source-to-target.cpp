a1629179171495cd22e732aa60826a1ef6150fdf
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2023-09-17 09:27:03 +0530

    Time: 11 ms (65.14%), Space: 12.4 MB (39.34%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> ans;
-        int n = graph.size();
-        int s = 0;
-        vector<int> path;
-        path.push_back(s);
-        queue<vector<int>> q;
-        q.push(path);
-        while(!q.empty()){
-            path = q.front();
-            q.pop();
-            int val = path.back();
-            if(val == n-1)
-                ans.push_back(path);
-            for(auto x: graph[val]){
-                vector<int> tmp(path);
-                tmp.push_back(x);
-                q.push(tmp);
-            }     
+    int target;
+    vector<vector<int>> res;
+    vector<int> tmp;
+    void dfs(vector<vector<int>>& graph, int currNode = 0){
+        tmp.push_back(currNode);
+        if(currNode == target)
+            res.push_back(tmp);
+        else{
+            for(auto x : graph[currNode])
+                dfs(graph, x);
         }
-        return ans;
+        tmp.pop_back();
     }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph)  {
+        target = graph.size() - 1;
+        dfs(graph);
+        return res;
+    }    
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+//         vector<vector<int>> ans;
+//         int n = graph.size();
+//         int s = 0;
+//         vector<int> path;
+//         path.push_back(s);
+//         queue<vector<int>> q;
+//         q.push(path);
+//         while(!q.empty()){
+//             path = q.front();
+//             q.pop();
+//             int val = path.back();
+//             if(val == n-1)
+//                 ans.push_back(path);
+//             for(auto x: graph[val]){
+//                 vector<int> tmp(path);
+//                 tmp.push_back(x);
+//                 q.push(tmp);
+//             }     
+//         }
+//         return ans;
+//     }
+// };
