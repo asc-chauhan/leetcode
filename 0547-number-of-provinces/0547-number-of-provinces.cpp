b9c1c77c7d5b526267b5266de04340837a88d9b5
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2023-09-25 09:37:32 +0530

    Time: 23 ms (54.69%), Space: 17.2 MB (12.58%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,25 +1,59 @@
 class Solution {
 public:
+void soln(vector<int>adj[],int node,vector<bool>&vis){
+    vis[node] = 1;
+    for(auto i:adj[node]){
+        if(!vis[i]){
+            soln(adj,i,vis);
+        }
+    }
+}
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> vis(n);
-        int cnt = 0;
-        queue<int> q;
-        for(int i = 0; i < n; i++){
-            if(vis[i] == 0){
-              cnt++;
-              q.push(i);
-              while(!q.empty()){
-                int j = q.front();
-                q.pop();
-                vis[j] = 1;
-                for(int k = 0; k < n; k++){
-                  if(isConnected[j][k] == 1 && vis[k]!=1 && k!=j)
-                    q.push(k);
+        vector<int>adj[n];
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(isConnected[i][j]){
+                adj[i].push_back(j);
+                adj[j].push_back(i);
                 }
-              }
             }
         }
-        return cnt;
+        vector<bool>vis(n,0);
+        int ans= 0;
+        for(int i = 0;i<n;i++){
+            if(!vis[i]){
+                soln(adj,i,vis);
+                ans++;
+            }
+        }
+        return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n = isConnected.size();
+//         vector<int> vis(n);
+//         int cnt = 0;
+//         queue<int> q;
+//         for(int i = 0; i < n; i++){
+//             if(vis[i] == 0){
+//               cnt++;
+//               q.push(i);
+//               while(!q.empty()){
+//                 int j = q.front();
+//                 vis[j] = 1;
+//                 q.pop();
+//                 for(int k = 0; k < n; k++){
+//                   if(isConnected[j][k] == 1 && vis[k]!=1 && k!=j)
+//                     q.push(k);
+//                 }
+//               }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
