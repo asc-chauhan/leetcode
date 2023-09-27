45a197d904924926fbfe7fdfdff4e1431da51c93
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2023-09-27 08:28:36 +0530

    Time: 4 ms (26.39%), Space: 6.9 MB (5.62%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,21 +1,44 @@
+
 class Solution {
-public:
-    int solve(int n) {
-        int sum = 0;
-        while(n > 0) {
-			int r = n%10;
-            sum += r*r;
-            n /= 10;
+private:
+    int nextNumber(int n){
+        int newNumber = 0;
+        while(n!=0){
+            int num = n%10;
+            newNumber += num*num;
+            n = n/10;
         }
-        return sum;
+        return newNumber;
     }
+public:
     bool isHappy(int n) {
-        int slow = n, fast = n;
-        do {
-            slow = solve(slow);
-            fast = solve(solve(fast));
-            if(fast == 1) return 1;
-        } while(slow != fast);
-        return 0;
+        unordered_set<int> set;
+        while(n!=1 && !set.count(n)){
+            set.insert(n);
+            n = nextNumber(n);
+        }
+        return n==1;
     }
 };
+
+// class Solution {
+// public:
+//     int solve(int n) {
+//         int sum = 0;
+//         while(n > 0) {
+// 			int r = n%10;
+//             sum += r*r;
+//             n /= 10;
+//         }
+//         return sum;
+//     }
+//     bool isHappy(int n) {
+//         int slow = n, fast = n;
+//         do {
+//             slow = solve(slow);
+//             fast = solve(solve(fast));
+//             if(fast == 1) return 1;
+//         } while(slow != fast);
+//         return 0;
+//     }
+// };
