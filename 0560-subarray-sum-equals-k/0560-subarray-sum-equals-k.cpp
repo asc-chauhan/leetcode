00066c1f76421c539d7ccc346420e48cc35690c4
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2023-10-19 16:07:44 +0530

    Time: 81 ms (37.89%), Space: 47.5 MB (21.32%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,16 @@
+//Solution 01: Prefix sum
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int preSum = 0, count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            preSum += nums[i];
+            int remove = preSum - k;
+            count += mp[remove];
+            mp[preSum] += 1;
+        }
+        return count;
+    }
+};
