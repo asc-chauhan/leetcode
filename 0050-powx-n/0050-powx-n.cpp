52533256cfb32603ddb2ddf0cc2f81eedec8bf9f
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2025-09-28 01:03:44 +0530

    Time: 0 ms (100%), Space: 5.9 MB (100%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long nn = n;
+        if(nn < 0)
+            nn = -1*nn;
+        while(nn){
+            if(nn%2){
+                ans = ans*x;
+                nn = nn-1;
+            }
+            else{
+                x = x*x;
+                nn = nn/2;
+            }
+        }
+        if(n < 0)
+            ans = (double)1.0/(double)ans;
+        return ans;
+    }
+};
+
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+//         double res=1.0;
+//         for(int i=n;i;i/=2){
+//             if(i&1)res*=x;
+//             x*=x;
+            
+//         }
+//         return n>=0?res:1/res; 
+//     }
+// };
