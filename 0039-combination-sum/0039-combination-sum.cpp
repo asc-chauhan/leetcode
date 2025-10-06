e6e6f1e30fbc98a9d3b3755d439df5617f509ccd
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2025-10-07 01:02:00 +0530

    Time: 11 ms (25.12%), Space: 17.7 MB (28.46%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,40 +1,65 @@
-// class Solution{
+class Solution{
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target){
+        vector<vector<vector<int>>> dp(target+1);
+        dp[0] = {{}};
+        for(int & x: candidates){
+            for(int i = x; i <= target; i++){
+                for(auto v: dp[i-x]){
+                    v.push_back(x);
+                    dp[i].push_back(v);
+                }
+            }
+        }
+        return dp[target];
+    }
+};
+
+
+// class Solution {
 // public:
-//     vector<vector<int>> combinationSum(vector<int>& candidates, int target){
-//         vector<vector<vector<int>>> dp(target+1);
-//         dp[0] = {{}};
-//         for(int & x: candidates){
-//             for(int i = x; i <= target; i++){
-//                 for(auto v: dp[i-x]){
-//                     v.push_back(x);
-//                     dp[i].push_back(v);
-//                 }
-//             }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<int> combination;
+//         vector<vector<int>> ans {};
+//         subCombinate(candidates, target, 0, combination, ans);
+//         return ans;
+//     }
+// private:
+//     void subCombinate(vector<int>& candidates, int target, int start, vector<int> & combination, vector<vector<int>> & ans){
+//         if (target < 0)
+//             return;
+//         if (target == 0){
+//             ans.push_back(combination);
+//             return;
 //         }
-//         return dp[target];
+//         for (int i = start; i < candidates.size(); i++){
+//             combination.push_back(candidates[i]);
+//             subCombinate(candidates, target - candidates[i], i, combination, ans);
+//             combination.pop_back();
+//         }
 //     }
 // };
 
-class Solution {
-public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> temp;
-        vector<vector<int>> ans;
-        backtrack(candidates, target, 0, temp, ans);
-        return ans;
-    }
-private:
-    void backtrack(vector<int>& candidates, int target, int start, vector<int> & temp, vector<vector<int>> & ans){
-        if (target < 0)
-            return;
-        if (target == 0){
-            ans.push_back(temp);
-            return;
-        }
-        for (int i = start; i < candidates.size(); i++){
-            temp.push_back(candidates[i]);
-            backtrack(candidates, target - candidates[i], i, temp, ans);
-            temp.pop_back();
-        }
-    }
-};
+
+// class Solution {
+// public:
+//     void findAnswer(vector<int>& candidates, vector<vector<int>>& ans, vector<int>& temp, int i, int target){
+//         if(target == 0){
+//             ans.push_back(temp);
+//             return;
+//         }
+//         if(i == candidates.size() || target - candidates[i] < 0)
+//             return;
+//         temp.push_back(candidates[i]);
+//         findAnswer(candidates, ans, temp, i, target-candidates[i]);
+//         temp.pop_back();
+//         findAnswer(candidates, ans, temp, i+1, target);
+//     }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         sort(candidates.begin(), candidates.end());
+//         vector<vector<int>> ans;
+//         vector<int> temp;
+//         findAnswer(candidates, ans, temp, 0, target);
+//         return ans;
+//     }
+// };
