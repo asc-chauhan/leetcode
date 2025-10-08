8d1e50b2d0fc3dd571d44e483b4b3a348defe0c6
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2025-10-08 12:51:26 +0530

    Time: 47 ms (39.82%), Space: 125.1 MB (65.71%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -16,7 +16,7 @@ public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success){
         sort(potions.begin(), potions.end());
         int n = spells.size();
-        vector<int> ans(n,0);
+        vector<int> ans(n);
         for(int i = 0; i < n; i++){
             int idx = bs(potions, spells[i], success);
             if(idx != -1)
