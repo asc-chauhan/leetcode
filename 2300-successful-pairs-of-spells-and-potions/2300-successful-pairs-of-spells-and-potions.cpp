a5d397a88b5ef902b73c3c895d02866a5a7b1fe5
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2025-10-08 12:49:51 +0530

    Time: 40 ms (75.85%), Space: 125 MB (81.16%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,16 +1,44 @@
-class Solution {
+class Solution{
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+    int bs(vector<int> & potions, long long strength, long long success){
+        int lo = 0, hi = potions.size()-1, idx = -1;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            if(1LL*potions[mid]*strength >= success){
+                idx = mid;
+                hi = mid-1;
+            }
+            else
+                lo = mid+1;
+        }
+        return idx;
+    }
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success){
         sort(potions.begin(), potions.end());
         int n = spells.size();
-        int m = potions.size();
-        vector<int> ans(n, 0);
+        vector<int> ans(n);
         for(int i = 0; i < n; i++){
-            long long s = spells[i];
-            long long need = (success+s-1)/s;
-            auto it = lower_bound(potions.begin(), potions.end(), need);
-            ans[i] = m - int(it - potions.begin());
+            int idx = bs(potions, spells[i], success);
+            if(idx != -1)
+                ans[i] = potions.size() - idx;
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+//         sort(potions.begin(), potions.end());
+//         int n = spells.size();
+//         int m = potions.size();
+//         vector<int> ans(n, 0);
+//         for(int i = 0; i < n; i++){
+//             long long s = spells[i];
+//             long long need = (success+s-1)/s;
+//             auto it = lower_bound(potions.begin(), potions.end(), need);
+//             ans[i] = m - int(it - potions.begin());
+//         }
+//         return ans;
+//     }
+// };
