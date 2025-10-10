924366cf912065fc3c4c32e6119165387496f8a3
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2025-10-11 01:56:31 +0530

    Time: 0 ms (100%), Space: 9.1 MB (90.84%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -5,7 +5,7 @@ public:
         dp[0] = 1;
         for(int i = 1; i <= target; i++){
             for(int j = 0; j < nums.size(); j++){
-                if(i - nums[j] >= 0)
+                if(nums[j] <= i)
                     dp[i] += dp[i-nums[j]];
             }
         }
