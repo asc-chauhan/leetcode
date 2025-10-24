9b0b8aa3d2c696d9a0186296dff83d21967f1a70
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2025-10-24 15:29:05 +0530

    Time: 1826 ms (13.31%), Space: 388.1 MB (6.13%) - LeetHub

--- 3347-maximum-frequency-of-an-element-after-performing-operations-ii/3347-maximum-frequency-of-an-element-after-performing-operations-ii.cpp
+++ 3347-maximum-frequency-of-an-element-after-performing-operations-ii/3347-maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -1,39 +1,53 @@
-class Solution {
+class Solution{
 public:
-    static int maxFrequency(vector<int>& nums, int k, int numOperations)    
-    {
-        const int n=nums.size();
-        sort(nums.begin(), nums.end());
-        
-        int ans=0, cnt=0;
-        int l=0, r=0;
-        // Consider nums[i]
-        for (int i=0; i<n; i++) {
-            const int x=nums[i], L=max(1,x-k), R=min(nums.back(), x+k);
-
-            // count freq
-            int f=1;
-            int j=i+1;
-            for (;j<n && nums[j]==x; j++) f++;
-            i=j-1; // nums[j]!=x
-
-            // move l
-            while (l<n && nums[l]<L) l++;
-
-            // move r
-            r=(r>i)?r:i; // start r from last position
-            while (r+1<n && nums[r+1]<=R) r++;
-
-            cnt=r-l+1;
-            ans=max(ans, f+min(cnt-f, numOperations));
+    int maxFrequency(vector<int> & nums, int k, int numOperations){
+        unordered_map<int, int> ranges;
+        unordered_map<int, int> freq;
+        set<int> points;
+        int preSum = 0;
+        for(auto x: nums){
+            freq[x]++;
+            ranges[x-k]++;
+            ranges[x+k+1]--;
+            points.insert(x);
+            points.insert(x-k); // left
+            points.insert(x+k+1); // right + 1
         }
-
-        // consider the subintervals with overlapping
-        for (l=0, r=0; r<n; r++) {
-            int x=nums[r], L=max(1, x-2*k);
-            while (l<r && nums[l]<L) l++;
-            ans=max(ans, min(r-l+1, numOperations));
+        int ans = 1;
+        for(auto x: points){
+            preSum += ranges[x];
+            int extra = min(numOperations, preSum - freq[x]);
+            ans = max(ans, extra + freq[x]);
         }
         return ans;
     }
 };
+
+// Memory Limit exceeded
+// class Solution{
+// public:
+//     int maxFrequency(vector<int> & nums, int k, int numOperations){
+//         int n = nums.size();
+//         int mini = INT_MAX, maxi = INT_MIN;
+//         int ans = 0;
+//         for(auto x: nums){
+//             mini = min(mini, x);
+//             maxi = max(maxi, x);
+//         }
+//         vector<int> freq(maxi+1, 0);
+//         for(auto x: nums)
+//             freq[x]++;
+//         for(int i = 1; i <= maxi; i++)
+//             freq[i] += freq[i-1];
+//         for(int curr = mini; curr <= maxi; curr++){
+//             int l = max(mini, curr - k);
+//             int r = min(maxi, curr + k);
+//             int currFreq = freq[curr] - freq[curr-1];
+//             // deleting freq of the curr from extra because we are trying to reach this curr
+//             int extra = min(numOperations, freq[r] - freq[l-1] - currFreq);
+//             ans = max(ans, extra + currFreq);
+//         }
+//         return ans;
+//     }
+// };
+
