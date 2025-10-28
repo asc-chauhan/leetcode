17244e305a4267afe29c040f207e5650774e9aca
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2025-10-29 01:25:27 +0530

    Time: 3 ms (82.86%), Space: 22.1 MB (68.1%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> leftSum(n, 0);
-        long long sum = nums[0];
-        leftSum[0] = nums[0];
-        for(int i = 1; i < n; i++){
-            leftSum[i] = leftSum[i-1] + nums[i];
-            sum += nums[i];
+        int len = nums.size(), count = 0, left = 0, right = 0;
+        for (int v : nums) 
+            right += v;
+        for (int i = 0; i < len; i++) {
+            left += nums[i];
+            right -= nums[i];
+            if (nums[i] != 0) 
+                continue;
+            if (left == right) 
+                count += 2;
+            if (abs(left - right) == 1) 
+                count++;
         }
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0 && leftSum[i] == sum - leftSum[i])
-                ans += 2;
-            else if(nums[i] == 0 && abs(leftSum[i]-sum+leftSum[i]) == 1)
-                ans += 1;
-        }
-        return ans;
+        return count;
     }
 };
+
+
+// extra space
+// class Solution {
+// public:
+//     int countValidSelections(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> leftSum(n, 0);
+//         long long sum = nums[0];
+//         leftSum[0] = nums[0];
+//         for(int i = 1; i < n; i++){
+//             leftSum[i] = leftSum[i-1] + nums[i];
+//             sum += nums[i];
+//         }
+//         int ans = 0;
+//         for(int i = 0; i < n; i++){
+//             if(nums[i] == 0 && leftSum[i] == sum - leftSum[i])
+//                 ans += 2;
+//             else if(nums[i] == 0 && abs(leftSum[i]-sum+leftSum[i]) == 1)
+//                 ans += 1;
+//         }
+//         return ans;
+//     }
+// };
