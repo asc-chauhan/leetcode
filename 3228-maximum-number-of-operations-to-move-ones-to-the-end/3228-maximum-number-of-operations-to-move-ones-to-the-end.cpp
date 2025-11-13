47b0314b113b3ccd839617a4ba337f7891a9ee2f
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2025-11-14 00:28:03 +0530

    Time: 4 ms (71.38%), Space: 15.7 MB (85.14%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -3,18 +3,13 @@ public:
     int maxOperations(string s) {
         int ans = 0;
         int n = s.size();
-        vector<int> cntOnes(n, 0);
         int cnt = 0;
         for(int i = 0; i < n; i++){
             if(s[i] == '1')
                 cnt++;
-            else
-                cntOnes[i] = cnt;
-            cout << cntOnes[i] << " ";
-        }
-        for(int i = n-1; i > 0; i--){
-            if(s[i] == '0' && s[i-1] == '1')
-                ans += cntOnes[i];
+            else if(i > 0  &&  s[i-1] == '1')
+                ans += cnt;
+            // cout << cntOnes[i] << " ";
         }
         return ans;
     }
