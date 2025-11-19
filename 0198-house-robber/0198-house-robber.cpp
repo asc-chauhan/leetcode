af6629a8d745e6641a08691776aef00afd97744c
asc-chauhan <125276661+asc-chauhan@users.noreply.github.com>
2025-11-20 00:54:34 +0530

    Time: 0 ms (100%), Space: 10.6 MB (45.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size()==0)return 0;
-        int n=nums.size();
-        vector<int> dp(n+1,0);
-        dp[1]=nums[0];
-        for(int i=2;i<=n;i++){
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
-        }
-        return dp[n];
+        int n = nums.size();
+        if(n == 1)
+            return nums[0];
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], nums[0]);
+        for(int i = 2; i < n; i++)
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+        return dp[n-1];
     }
 };
-
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-//         int n = nums.size();
-//         int dp[n];
-//         if(n == 1)
-//             return nums[0];
-//         dp[0] = nums[0];
-//         dp[1] = nums[1];
-//         int res = max(dp[0],dp[1]);
-//         for(int i = 2; i < n; i++){
-//             dp[i] = nums[i];
-//             int m = dp[0];
-//             for(int j = 0; j < i-1; j++)
-//                 m = max(m, dp[j]);
-//             dp[i] += m;
-//             res = max(res, dp[i]);
-//         }
-//         return res;
-//     }
-// };
